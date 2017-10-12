11d96e1afc2b18243f240bd09517033a0c71e89b
Andreas Norstein <andrnors@stud.ntnu.no>
2017-10-12 16:56:34 +0200

    no clue

--- assignment.py
+++ assignment.py
@@ -5,24 +5,27 @@ import random
 import codecs
 import string
 import gensim
+from nltk.stem.porter import PorterStemmer
 
 RANDOM_NUMBER = random.seed(123)
-F = codecs.open("book.txt", 'r')
+F = codecs.open("book.txt", 'r', 'utf-8')
 
 
 def partitions(input_file, partion_factor):
     """partition file into paragraphs"""
+    print "Partitions file to paragraphs"
     return input_file.read().split(partion_factor)
 
-PARTITIONS = partitions(F, "\n\n")
+# PARTITIONS = partitions(F, "\n\n")
 STOPWORDS = partitions(codecs.open("common_words.txt", "r"), ",")
 
 
 def filter_partitions(partitions_list):
     """filter partitions by removing the word Gutenberg"""
-    return [p for p in partitions_list if "Gutenberg" not in p]
+    print "Removes the word Gutenberg"
+    return [p for p in partitions_list if "Gutenberg".lower() not in p.lower()]
 
-FILTERD_PARTITION = filter_partitions(PARTITIONS)
+# FILTERD_PARTITION = filter_partitions(PARTITIONS)
 
 
 def tokenize_paragraphs(filter_partitions_list):
@@ -30,7 +33,8 @@ def tokenize_paragraphs(filter_partitions_list):
     tokenize paragraphs by splitting them into words.
     Returns a list of paragraphs where each paragraph is a list of words
     """
-    exclude = set(string.punctuation)
+    print "Remove punctuations"
+    exclude = set(string.punctuation + "\n\r\t")
     temp_tokens = [p.split(" ") for p in filter_partitions_list]
     tokens = []
     for token in temp_tokens:
@@ -43,18 +47,34 @@ def tokenize_paragraphs(filter_partitions_list):
             tokens.append(stem_tokens)
     return tokens
 
-TOKENIZE_PARAGAPHS = tokenize_paragraphs(FILTERD_PARTITION)
+# TOKENIZE_PARAGAPHS = tokenize_paragraphs(FILTERD_PARTITION)
 
 
-def generate_dictionary(tokenized_paragraphs):
+def stem(paragraphs):
+    print "Stemming"
+    stemmer = PorterStemmer()
+    temp_paragraphs = []
+    for paragraph in paragraphs:
+        temp_paragraph = []
+        for word in paragraph:
+            temp_paragraph.append(stemmer.stem(word.lower()))
+        temp_paragraphs.append(temp_paragraph)
+    return temp_paragraphs
+
+# STEMMED_AND_TOKENIZED_PARAGRAPHS = stem(TOKENIZE_PARAGAPHS)
+
+
+def generate_dictionary(tokenized_and_stemmed_paragraphs):
     """Generate Dictionary based on tokenized paragraphs"""
-    return gensim.corpora.Dictionary(tokenized_paragraphs)
+    print "Generate Dictionary"
+    return gensim.corpora.Dictionary(tokenized_and_stemmed_paragraphs)
 
-DICTONARY = generate_dictionary(TOKENIZE_PARAGAPHS)
+# DICTONARY = generate_dictionary(STEMMED_AND_TOKENIZED_PARAGRAPHS)
 
 
 def filter_stopwords(dictionary):
     """Filter out all stopwords"""
+    print "Filters Stopwords"
     stop_ids = []
     for word in STOPWORDS:
         if word == STOPWORDS[-1].strip("\n"):
@@ -70,59 +90,61 @@ def filter_stopwords(dictionary):
     dictionary.compactify()
     return dictionary
 
-DICTIONARY_FILTERD = filter_stopwords(DICTONARY)
+# DICTIONARY_FILTERED = filter_stopwords(DICTONARY)
 
 
-def map_to_bow(dictionary, tokenized_paragraphs):
+def map_to_bow(dictionary, tokenized_and_stemmed_paragraphs):
     """ Map paragraphs into Bags-of-Words using a dictionary"""
-    corpus = []
-    for document in tokenized_paragraphs:
-        corpus.append(dictionary.doc2bow(document))
-    return corpus
+    print "Generates bag of words"
+    return [dictionary.doc2bow(document) for document in tokenized_and_stemmed_paragraphs]
 
-CORPUS = map_to_bow(DICTIONARY_FILTERD, TOKENIZE_PARAGAPHS)
+# CORPUS = map_to_bow(DICTIONARY_FILTERED, STEMMED_AND_TOKENIZED_PARAGRAPHS)
 
 
 def build_tf_idf_model(corpus):
     """  Build TF-IDF model using corpus (list of paragraphs) from the previous part"""
+    print "Builds td-idf model"
     return gensim.models.TfidfModel(corpus)
 
-TFIDF_MODEL = build_tf_idf_model(CORPUS)
+# TFIDF_MODEL = build_tf_idf_model(CORPUS)
 
 
 def map_bow_to_tfidf_weights(tfidf_model, bow):
     """ Map Bags-of-Words into TF-IDF weights"""
+    print "Create tf-idf weights"
     return [tfidf_model[b] for b in bow]
 
-TFIDF_CORPUS = map_bow_to_tfidf_weights(TFIDF_MODEL, CORPUS)
+
+# TFIDF_CORPUS = map_bow_to_tfidf_weights(TFIDF_MODEL, CORPUS)
 
 
 def construct_matrix_similarity(corpus, tfidf_model):
     """
     Construct MatrixSimilarity object that calculate similarities between paragraphs and queries:
     """
+    print "Constructs matrix similarity"
     return gensim.similarities.MatrixSimilarity(tfidf_model[corpus])
 
-MATRIX_SIMILARITY = construct_matrix_similarity(TFIDF_CORPUS, TFIDF_MODEL)
+# MATRIX_SIMILARITY = construct_matrix_similarity(TFIDF_CORPUS, TFIDF_MODEL)
 
 
 def build_lsi_model(dictionary, corpus, num_topics):
     """ LSI model using as an input the corpus with TF-IDF weights. Set
     number of topics to 100. In the end, each paragraph should be represented
     with a list of 100 pairs (topic-index, LSI-topic-weight) ). """
-    lsi_model = gensim.models.LsiModel(corpus, id2word=dictionary, num_topics=num_topics)
-    return lsi_model
+    print "Build lsi model"
+    return gensim.models.LsiModel(corpus, id2word=dictionary, num_topics=num_topics)
 
-LSI_MODEL = build_lsi_model(DICTIONARY_FILTERD, TFIDF_CORPUS, 100)
+# LSI_MODEL = build_lsi_model(DICTIONARY_FILTERD, TFIDF_CORPUS, 100)
 
 
 def create_lsi_corpus(model, corpus):
     """ creatse lsi_corpus"""
+    print "Create lsi corpus"
     return [model[b] for b in corpus]
 
-LSI_CORPUS = create_lsi_corpus(LSI_MODEL, TFIDF_CORPUS)
+# LSI_CORPUS = create_lsi_corpus(LSI_MODEL, TFIDF_CORPUS)
+# LSI_MATRIX_SIMILARITY = construct_matrix_similarity(LSI_CORPUS, LSI_MODEL)
 
-LSI_MATRIX_SIMILARITY = construct_matrix_similarity(LSI_CORPUS, LSI_MODEL)
-
-
+F.close()
 ######################## PART 4 ################################
--- part4.py
+++ part4.py
@@ -9,18 +9,23 @@ def preprocessing(query):
     :rtype: list
     :returns list with filtered_query, tokenized_query, dictionary, filtered_dictionary and bag_of_words
     """
-    filter_query = filter_partitions([query])
+    filter_query = filter_partitions(query)
     tokenized_query = tokenize_paragraphs(filter_query)
-    dictionary = generate_dictionary(tokenized_query)
+    stemmed_query = stem(tokenized_query)
+    dictionary = generate_dictionary(stemmed_query)
     filtered_dictionary = filter_stopwords(dictionary)
-    bow = map_to_bow(filtered_dictionary, tokenized_query)
+    bow = map_to_bow(filtered_dictionary, stemmed_query)
     
     return [filter_query, tokenized_query, dictionary, filtered_dictionary, bow]
 
-preprocessed = preprocessing("What is the function of money?")
+preprocessed = preprocessing(["What is the function of money?"])
 
-filtered_query, tokenized_query, dictionary, filtered_dictionary, bow = preprocessed[0], preprocessed[1], preprocessed[2], preprocessed[3], preprocessed[4]
+# ex2 = preprocessing(["How taxes influence Economics?"])
 
+# filtered_query, tokenized_query, dictionary, filtered_dictionary, corpus_bow = ex2[0], ex2[1], ex2[2], ex2[3], ex2[4]
+filtered_query, tokenized_query, dictionary, filtered_dictionary, corpus_bow = preprocessed[0], preprocessed[1], preprocessed[2], preprocessed[3], preprocessed[4]
+print corpus_bow
+print dictionary
 
 # 4.2 Convert BOW to TF-IDF representation. Report TF-IDF weights. For example, for the query "How
 # taxes influence Economics?" TF-IDF weights are:
@@ -28,4 +33,8 @@ filtered_query, tokenized_query, dictionary, filtered_dictionary, bow = preproce
 # Some useful code:
 # $ ... = tfidf_model[...]
 
+# tfidf_model = build_tf_idf_model(corpus_bow)
+#
+# tfidf_weights = map_bow_to_tfidf_weights(tfidf_model, corpus_bow)
 
+# print tfidf_weights
