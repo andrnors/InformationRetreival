b8dec4006086ed2eba5c7818993045153a8f0b14
Andreas Norstein <andrnors@stud.ntnu.no>
2017-10-16 15:59:41 +0200

    done

--- assignment.py
+++ assignment.py
@@ -16,7 +16,7 @@ def partitions(input_file, partion_factor):
     print "Partitions file to paragraphs"
     return input_file.read().split(partion_factor)
 
-# PARTITIONS = partitions(F, "\n\n")
+PARTITIONS = partitions(F, "\n\n")
 STOPWORDS = partitions(codecs.open("common_words.txt", "r"), ",")
 
 
@@ -25,7 +25,7 @@ def filter_partitions(partitions_list):
     print "Removes the word Gutenberg"
     return [p for p in partitions_list if "Gutenberg".lower() not in p.lower()]
 
-# FILTERD_PARTITION = filter_partitions(PARTITIONS)
+FILTERD_PARTITION = filter_partitions(PARTITIONS)
 
 
 def tokenize_paragraphs(filter_partitions_list):
@@ -47,7 +47,7 @@ def tokenize_paragraphs(filter_partitions_list):
             tokens.append(stem_tokens)
     return tokens
 
-# TOKENIZE_PARAGAPHS = tokenize_paragraphs(FILTERD_PARTITION)
+TOKENIZE_PARAGAPHS = tokenize_paragraphs(FILTERD_PARTITION)
 
 
 def stem(paragraphs):
@@ -61,7 +61,7 @@ def stem(paragraphs):
         temp_paragraphs.append(temp_paragraph)
     return temp_paragraphs
 
-# STEMMED_AND_TOKENIZED_PARAGRAPHS = stem(TOKENIZE_PARAGAPHS)
+STEMMED_AND_TOKENIZED_PARAGRAPHS = stem(TOKENIZE_PARAGAPHS)
 
 
 def generate_dictionary(tokenized_and_stemmed_paragraphs):
@@ -69,7 +69,7 @@ def generate_dictionary(tokenized_and_stemmed_paragraphs):
     print "Generate Dictionary"
     return gensim.corpora.Dictionary(tokenized_and_stemmed_paragraphs)
 
-# DICTONARY = generate_dictionary(STEMMED_AND_TOKENIZED_PARAGRAPHS)
+DICTONARY = generate_dictionary(STEMMED_AND_TOKENIZED_PARAGRAPHS)
 
 
 def filter_stopwords(dictionary):
@@ -90,7 +90,7 @@ def filter_stopwords(dictionary):
     dictionary.compactify()
     return dictionary
 
-# DICTIONARY_FILTERED = filter_stopwords(DICTONARY)
+DICTIONARY_FILTERED = filter_stopwords(DICTONARY)
 
 
 def map_to_bow(dictionary, tokenized_and_stemmed_paragraphs):
@@ -98,7 +98,7 @@ def map_to_bow(dictionary, tokenized_and_stemmed_paragraphs):
     print "Generates bag of words"
     return [dictionary.doc2bow(document) for document in tokenized_and_stemmed_paragraphs]
 
-# CORPUS = map_to_bow(DICTIONARY_FILTERED, STEMMED_AND_TOKENIZED_PARAGRAPHS)
+CORPUS = map_to_bow(DICTIONARY_FILTERED, STEMMED_AND_TOKENIZED_PARAGRAPHS)
 
 
 def build_tf_idf_model(corpus):
@@ -106,26 +106,26 @@ def build_tf_idf_model(corpus):
     print "Builds td-idf model"
     return gensim.models.TfidfModel(corpus)
 
-# TFIDF_MODEL = build_tf_idf_model(CORPUS)
+TFIDF_MODEL = build_tf_idf_model(CORPUS)
 
 
 def map_bow_to_tfidf_weights(tfidf_model, bow):
     """ Map Bags-of-Words into TF-IDF weights"""
     print "Create tf-idf weights"
-    return [tfidf_model[b] for b in bow]
+    return tfidf_model[bow]
 
 
-# TFIDF_CORPUS = map_bow_to_tfidf_weights(TFIDF_MODEL, CORPUS)
+TFIDF_CORPUS = map_bow_to_tfidf_weights(TFIDF_MODEL, CORPUS)
 
 
-def construct_matrix_similarity(corpus, tfidf_model):
+def construct_matrix_similarity(corpus):
     """
     Construct MatrixSimilarity object that calculate similarities between paragraphs and queries:
     """
     print "Constructs matrix similarity"
-    return gensim.similarities.MatrixSimilarity(tfidf_model[corpus])
+    return gensim.similarities.MatrixSimilarity(corpus)
 
-# MATRIX_SIMILARITY = construct_matrix_similarity(TFIDF_CORPUS, TFIDF_MODEL)
+TFIDF_MATRIX_SIMILARITY = construct_matrix_similarity(TFIDF_CORPUS)
 
 
 def build_lsi_model(dictionary, corpus, num_topics):
@@ -135,16 +135,87 @@ def build_lsi_model(dictionary, corpus, num_topics):
     print "Build lsi model"
     return gensim.models.LsiModel(corpus, id2word=dictionary, num_topics=num_topics)
 
-# LSI_MODEL = build_lsi_model(DICTIONARY_FILTERD, TFIDF_CORPUS, 100)
+LSI_MODEL = build_lsi_model(DICTIONARY_FILTERED, TFIDF_CORPUS, 100)
 
 
 def create_lsi_corpus(model, corpus):
-    """ creatse lsi_corpus"""
+    """ creates lsi_corpus"""
     print "Create lsi corpus"
-    return [model[b] for b in corpus]
+    return model[corpus]
 
-# LSI_CORPUS = create_lsi_corpus(LSI_MODEL, TFIDF_CORPUS)
-# LSI_MATRIX_SIMILARITY = construct_matrix_similarity(LSI_CORPUS, LSI_MODEL)
+LSI_CORPUS = create_lsi_corpus(LSI_MODEL, CORPUS)
+LSI_MATRIX_SIMILARITY = construct_matrix_similarity(LSI_CORPUS)
 
-F.close()
-######################## PART 4 ################################
+
+"""
+Part 4 of assignment
+"""
+from assignment import *
+
+queries = [["What is the function of money?"], ["How taxes influence Economics?"]]
+
+
+def preprocessing(query):
+    """preprocessing
+    :rtype: list
+    :returns list with filtered_query, tokenized_query, dictionary, filtered_dictionary and bag_of_words
+    """
+    print "preprocessing \n"
+    filter_query = filter_partitions(query)
+    tokenized_query = tokenize_paragraphs(filter_query)
+    stemmed_query = stem(tokenized_query)
+    bow = map_to_bow(DICTIONARY_FILTERED, stemmed_query)[0]
+    return [filter_query, tokenized_query, stemmed_query, bow]
+
+ex2 = preprocessing(queries[1])
+
+filtered_query, tokenized_query, stemmed_query, query_corpus_bow = ex2[0], ex2[1], ex2[2], ex2[3]
+
+# 4.2 Convert BOW to TF-IDF representation. Report TF-IDF weights. For example, for the query "How
+# taxes influence Economics?" TF-IDF weights are:
+# (tax: 0.26, econom: 0.82, influenc: 0.52)
+# Some useful code:
+# $ ... = tfidf_model[...]
+
+tfidf_weights = map_bow_to_tfidf_weights(TFIDF_MODEL, query_corpus_bow)
+print "TF-IDF weight:", tfidf_weights
+
+# 4.3 Report top 3 the most relevant paragraphs for the query "What is the function of
+# money?" according to TF-IDF model (displayed paragraphs should be in the original
+# form before
+# processing, but truncated up to first 5 lines).
+
+
+doc2similarity = enumerate(TFIDF_MATRIX_SIMILARITY[tfidf_weights])
+
+query_paragraphs = sorted(doc2similarity, key=lambda kv: -kv[1])[:3]
+for p in query_paragraphs:
+    print
+    print "Paragraph: " + str(p[0]) + ", similarity: " + str(p[1])
+    # print FILTERD_PARTITION[p[0]]
+
+
+# 4.4 Convert query TF-IDF representation
+# (for the query "What is the function of money?")
+# into LSI-topics representation (weights).
+# Report top 3. topics with the most significant (with the largest
+# absolute values) weights and top 3. the most relevant
+# paragraphs according to LSI model. Compare
+# retrieved paragraphs with the paragraphs found for TF-IDF model.
+
+ex1 = preprocessing(queries[0])
+filtered_query_0, tokenized_query_0, stemmed_query_0, query_corpus_bow_0 = ex1[0], ex1[1], ex1[2], ex1[3]
+
+lsi_query = create_lsi_corpus(LSI_MODEL, tfidf_weights)
+lsi_topics = sorted(lsi_query, key=lambda kv: -abs(kv[1]))[:3]
+doc2similarity_lsi = enumerate(LSI_MATRIX_SIMILARITY[lsi_query])
+top_paragraphs = sorted(doc2similarity_lsi, key=lambda kv: -kv[1])[:3]
+for topic in lsi_topics:
+    print
+    print "Topic: " + str(topic[0]) + ", value: " + str(topic[1])
+    print unicode(LSI_MODEL.show_topic(topic[0]))
+
+for pg in top_paragraphs:
+    print
+    print "Topic: " + str(pg[0]) + ", value: " + str(pg[1])
+    print FILTERD_PARTITION[pg[0]]
--- part4.py
+++ part4.py
@@ -3,29 +3,24 @@ Part 4 of assignment
 """
 from assignment import *
 
+queries = [["What is the function of money?"], ["How taxes influence Economics?"]]
+
 
 def preprocessing(query):
     """preprocessing 
     :rtype: list
     :returns list with filtered_query, tokenized_query, dictionary, filtered_dictionary and bag_of_words
     """
+    print "preprocessing \n"
     filter_query = filter_partitions(query)
     tokenized_query = tokenize_paragraphs(filter_query)
     stemmed_query = stem(tokenized_query)
-    dictionary = generate_dictionary(stemmed_query)
-    filtered_dictionary = filter_stopwords(dictionary)
-    bow = map_to_bow(filtered_dictionary, stemmed_query)
-    
-    return [filter_query, tokenized_query, dictionary, filtered_dictionary, bow]
+    bow = map_to_bow(DICTIONARY_FILTERED, stemmed_query)[0]
+    return [filter_query, tokenized_query, stemmed_query, bow]
 
-preprocessed = preprocessing(["What is the function of money?"])
+ex2 = preprocessing(queries[1])
 
-# ex2 = preprocessing(["How taxes influence Economics?"])
-
-# filtered_query, tokenized_query, dictionary, filtered_dictionary, corpus_bow = ex2[0], ex2[1], ex2[2], ex2[3], ex2[4]
-filtered_query, tokenized_query, dictionary, filtered_dictionary, corpus_bow = preprocessed[0], preprocessed[1], preprocessed[2], preprocessed[3], preprocessed[4]
-print corpus_bow
-print dictionary
+filtered_query, tokenized_query, stemmed_query, query_corpus_bow = ex2[0], ex2[1], ex2[2], ex2[3]
 
 # 4.2 Convert BOW to TF-IDF representation. Report TF-IDF weights. For example, for the query "How
 # taxes influence Economics?" TF-IDF weights are:
@@ -33,8 +28,45 @@ print dictionary
 # Some useful code:
 # $ ... = tfidf_model[...]
 
-# tfidf_model = build_tf_idf_model(corpus_bow)
-#
-# tfidf_weights = map_bow_to_tfidf_weights(tfidf_model, corpus_bow)
+tfidf_weights = map_bow_to_tfidf_weights(TFIDF_MODEL, query_corpus_bow)
+print "TF-IDF weight:", tfidf_weights
 
-# print tfidf_weights
+# 4.3 Report top 3 the most relevant paragraphs for the query "What is the function of
+# money?" according to TF-IDF model (displayed paragraphs should be in the original
+# form before
+# processing, but truncated up to first 5 lines).
+
+
+doc2similarity = enumerate(TFIDF_MATRIX_SIMILARITY[tfidf_weights])
+
+query_paragraphs = sorted(doc2similarity, key=lambda kv: -kv[1])[:3]
+for p in query_paragraphs:
+    print
+    print "Paragraph: " + str(p[0]) + ", similarity: " + str(p[1])
+    # print FILTERD_PARTITION[p[0]]
+
+
+# 4.4 Convert query TF-IDF representation
+# (for the query "What is the function of money?")
+# into LSI-topics representation (weights).
+# Report top 3. topics with the most significant (with the largest
+# absolute values) weights and top 3. the most relevant
+# paragraphs according to LSI model. Compare
+# retrieved paragraphs with the paragraphs found for TF-IDF model.
+
+ex1 = preprocessing(queries[0])
+filtered_query_0, tokenized_query_0, stemmed_query_0, query_corpus_bow_0 = ex1[0], ex1[1], ex1[2], ex1[3]
+
+lsi_query = create_lsi_corpus(LSI_MODEL, tfidf_weights)
+lsi_topics = sorted(lsi_query, key=lambda kv: -abs(kv[1]))[:3]
+doc2similarity_lsi = enumerate(LSI_MATRIX_SIMILARITY[lsi_query])
+top_paragraphs = sorted(doc2similarity_lsi, key=lambda kv: -kv[1])[:3]
+for topic in lsi_topics:
+    print
+    print "Topic: " + str(topic[0]) + ", value: " + str(topic[1])
+    print unicode(LSI_MODEL.show_topic(topic[0]))
+
+for pg in top_paragraphs:
+    print
+    print "Topic: " + str(pg[0]) + ", value: " + str(pg[1])
+    print FILTERD_PARTITION[pg[0]]
